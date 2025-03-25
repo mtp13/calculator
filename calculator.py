4911f7a517320877aed21a3aee39bf91d19fa8f3
Mike Pullen <mike.pullen@gmail.com>
2025-03-24 20:27:21 -0500

    move operations to operations.py

--- calculator.py
+++ calculator.py
@@ -1,23 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox, ttk
-
-
-def add(a, b):
-    return a + b
-
-
-def subtract(a, b):
-    return a - b
-
-
-def multiply(a, b):
-    return a * b
-
-
-def divide(a, b):
-    if b == 0:
-        return "Error: Division by zero"
-    return a / b
+# from operations import add, subtract, multiply, divide
 
 
 def on_button_click(value):
